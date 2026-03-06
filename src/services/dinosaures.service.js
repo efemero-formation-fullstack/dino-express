7bfc471f725d585b1ad5c165931ed00463e354b8
marnaroxwd <maxime.baldan11@gmail.com>
2026-03-06 13:18:10 +0100

    début listing

--- src/services/dinosaures.service.js
+++ src/services/dinosaures.service.js
@@ -1,7 +1,32 @@
+import { Op } from "sequelize";
+
 const dinoService = {
   create: async () => {},
   delete: async () => {},
-  getAll: async () => {},
+  getAll: async (filter, pagination) => {
+    const where = {};
+
+    if (filter) {
+      if (filter.name) {
+        where.name = {
+          [Op.iLike]: `%${filter.name}%`,
+        };
+      }
+      if (filter.regime) {
+        where.regime = filter.regime;
+      }
+      if (filter.periode) {
+        where.periode = filter.periode;
+      }
+    }
+    const order = [];
+    if (pagination.orders) {
+      if (pagination.orders.name) {
+        order.push(["name", pagination.orders.name]);
+      }
+    }
+    const dinosaures = await debug.Dinosaures.findAll();
+  },
   getById: async () => {},
 };
 
